e8c3c21e467404e599f9df60a43f7d956dde378a
Luis97Lopez <lpzr97@gmail.com>
2020-10-20 21:05:28 -0500

    Ahora si se ignora la carpeta de Node Modules

---
+++ citas/src/App.js
@@ -0,0 +1,38 @@
+import React, {Fragment, useState} from 'react';
+import Formulario from './components/Formulario'
+
+
+function App() {
+  
+  // Arreglo de Citas
+  const [citas, guardarCitas] = useState([])
+
+  // Funcion que tome las citas actuales y agregue la nueva
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ])
+  }
+
+  return (
+    <Fragment>
+    <h1> Administrador de pacientes </h1>
+
+    <div className="container"> 
+      <div className="row">
+        <div className="one-half column">
+          <Formulario 
+            crearCita={crearCita}
+          />
+        </div>
+        <div className="one-half column">
+          2
+        </div>
+      </div>
+    </div>
+    </Fragment>
+  );
+}
+
+export default App;
